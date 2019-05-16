cab07838bf37c4460a17b532fb6795b1853468bc
na-wu <nwu1018@gmail.com>
2019-05-16 11:23:34 -0700

    clean up ignored files

--- TwitchTextDataPython.py
+++ TwitchTextDataPython.py
@@ -3,47 +3,42 @@
 import socket
 import logging
 
-
+import config
 # Configure our logs to a specific format #
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d_%H:%M:%S',
                     handlers=[logging.FileHandler('chat.log')])
 
-
-
 #=== First we define our constants ===#
-server = 'irc.chat.twitch.tv' #server client we're connecting to
-port = 6667 # port we're going to use
-nickname = 'sweetenedyams' # my nickname
-token = '' #my twitch authentication token
-channel = '#ninja' #Twitch Streamer we are going to be watching
-
-
-
+HOST = config.server #HOST client we're connecting to
+PORT = 6667 # port we're going to use
+NICKNAME = config.nickname #NICKNAME
+TOKEN = config.token #twitch authentication TOKEN
+CHANNEL = config.channel #Twitch Streamer we are going to be watching
 
 #initialize a socket
-
 sock = socket.socket()
-
-
-#connect the socket to the server via the port
-sock.connect(('irc.chat.twitch.tv', 6667))
-
-
-
-
-
-
-#Time to send our token and nickname to the IRC server as encoded strings
+#connect the socket to the HOST via PORT 6667
+sock.connect((HOST, PORT))
+#Send TOKEN and NICKNAME to the IRC HOST as encoded strings
 #   for authentication
-sock.send(u"PASS {token}\n".encode('utf-8'))
-sock.send(u"NICK {nickname}\n".encode('utf-8'))
-sock.send(u"JOIN {channel}\n".encode('utf-8'))
+sock.send(f"PASS {TOKEN}\n".encode('utf-8'))
+sock.send(f"NICK {NICKNAME}\n".encode('utf-8'))
+sock.send(f"JOIN {CHANNEL}\n".encode('utf-8'))
 
 
-# Decode and receive the channel message(s)
+# Decode and receive the CHANNEL message(s)
+while True:
+    resp = sock.recv(2048).decode('utf-8')
+    if resp.startswith('PING'):
+        print(resp)
+        logging.info(resp)
+        sock.close()
+    print(resp)
+    logging.info(resp)
 
-resp = sock.recv(2048)
-logging.info(resp)
-    
+
+sock.close()
+
+
