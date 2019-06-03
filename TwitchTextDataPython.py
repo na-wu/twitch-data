3e85dcb2aa746db366decfdbd7061f1059448058
na-wu <nwu1018@gmail.com>
2019-06-03 15:10:45 -0700

    add dataframe module to visualize chat log

--- TwitchTextDataPython.py
+++ TwitchTextDataPython.py
@@ -2,6 +2,9 @@
 
 import socket
 import logging
+from datetime import datetime
+import re
+
 
 import config
 # Configure our logs to a specific format #
@@ -27,14 +30,11 @@ sock.send(f"PASS {TOKEN}\n".encode('utf-8'))
 sock.send(f"NICK {NICKNAME}\n".encode('utf-8'))
 sock.send(f"JOIN {CHANNEL}\n".encode('utf-8'))
 
-
 # Decode and receive the CHANNEL message(s)
 while True:
     resp = sock.recv(2048).decode('utf-8')
     if resp.startswith('PING'):
-        print(resp)
-        logging.info(resp)
-        sock.close()
+        sock.send('PONG\n'.encode('utf-8'))
     print(resp)
     logging.info(resp)
 
---
+++ dataframe.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from datetime import datetime
+import re
+
+
+
+def get_chat_dataframe(file):
+    data = []
+
+    with open(file, 'r', encoding='utf-8') as f:
+        lines = f.read().split('\n\n')
+        
+        for line in lines:
+            try:
+                time_logged = line.split()[0].strip()
+                time_logged = datetime.strptime(time_logged, '%Y-%m-%d_%H:%M:%S')
+
+                username_message = line.split()[1:]
+                username_message = ''.join(username_message).strip()
+
+                username, channel, message = re.search(
+                    ':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)', username_message
+                ).groups()
+
+                d = {
+                    'dt': time_logged,
+                    'channel': channel,
+                    'username': username,
+                    'message': message
+                }
+
+                data.append(d)
+                print("hello")
+            
+            except Exception:
+                pass
+            
+    return pd.DataFrame().from_records(data)
+        
+    
+df = get_chat_dataframe('chat.log')
+print(df.shape)
+df.head()
